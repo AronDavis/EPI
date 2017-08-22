faa46e04f1e8f1fbad6ea88a40d1a558550cf976
Aron Davis <arontd@gmail.com>
2017-08-22 18:30:55 -0400

    6.1

---
+++ Solutions/_6/_6_1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Solutions._6
+{
+    /// <summary>
+    /// How would you compute the parity of a very large number of 64-bit words?  The parity of a binary word is 1 if the number of 1s is odd.  Otherwise it is 0.
+    /// </summary>
+    public class _6_1
+    {
+        public static void Run(int pivotIndex, int[] data)
+        {
+            int pivot = data[pivotIndex];
+
+            int smaller = 0;
+            int equal = 0;
+            int larger = data.Length - 1;
+
+            while(equal <= larger)
+            {
+                if (data[equal] < pivot)
+                    swap(smaller++, equal++, data);
+                else if (data[equal] == pivot)
+                    ++equal;
+                else
+                    swap(equal, larger--, data);
+            }
+        }
+
+        private static void swap(int i, int j, int[] data)
+        {
+            int temp = data[i];
+            data[i] = data[j];
+            data[j] = temp;
+        }
+    }
+}
---
+++ Tests/_6/_6_1_Tests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solutions._6;
+
+namespace Tests
+{
+    [TestClass]
+    public class _6_1_Tests
+    {
+        [TestMethod]
+        public void Test()
+        {
+            int[] data = new int[]
+            {
+                3, 1, 2, 1, 3, 1
+            };
+
+            _6_1.Run(2, data);
+
+            Assert.IsTrue(1 == data[0]);
+            Assert.IsTrue(1 == data[1]);
+            Assert.IsTrue(1 == data[2]);
+            Assert.IsTrue(2 == data[3]);
+            Assert.IsTrue(3 == data[4]);
+            Assert.IsTrue(3 == data[5]);
+        }
+    }
+}
