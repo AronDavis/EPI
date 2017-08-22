d85f537e1b4bd7e0160d87ed93c3a078e3086f5c
Aron Davis <arontd@gmail.com>
2017-08-22 17:22:44 -0400

    5.5

---
+++ Solutions/_5/_5_5.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Solutions._5
+{
+    /// <summary>
+    /// Write a function that multiplies two unsigned positive integers.  The only operators allowed are assignment and bitwise.
+    /// </summary>
+    public class _5_5
+    {
+        public static uint Run(uint num1, uint num2)
+        {
+            uint sum = 0;
+            while(num1 > 0)
+            {
+                //examine every bit in num1
+                if((num1 & 1) > 0)
+                    sum = add(sum, num2);
+
+                num1 >>= 1;
+                num2 <<= 1;
+            }
+            return sum;
+        }
+
+        private static uint add(uint num1, uint num2)
+        {
+            uint sum = 0;
+            uint carryin = 0;
+            uint k = 1;
+            uint temp1 = num1;
+            uint temp2 = num2;
+
+            while(temp1 > 0 || temp2 > 0)
+            {
+                uint k1 = num1 & k;
+                uint k2 = num2 & k;
+
+                uint carryout = (k1 & k2) | (k1 & carryin) | (k2 & carryin);
+                sum |= (k1 ^ k2 ^ carryin);
+                carryin = carryout << 1;
+                k <<= 1;
+                temp1 >>= 1;
+                temp2 >>= 1;
+            }
+
+            return sum | carryin;
+        }
+    }
+}
---
+++ Tests/_5/_5_5_Tests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solutions._5;
+
+namespace Tests
+{
+    [TestClass]
+    public class _5_5_Tests
+    {
+        [TestMethod]
+        public void Test()
+        {
+            Assert.IsTrue(_5_5.Run(0, 0) == 0);
+
+            Assert.IsTrue(_5_5.Run(0, 1) == 0);
+
+            Assert.IsTrue(_5_5.Run(1, 0) == 0);
+
+            Assert.IsTrue(_5_5.Run(1, 1) == 1);
+
+            Assert.IsTrue(_5_5.Run(3, 7) == 21);
+
+            Assert.IsTrue(_5_5.Run(7, 3) == 21);
+        }
+    }
+}
