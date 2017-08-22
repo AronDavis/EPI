73022577c6badd579d48ed178e1052055a9d73ba
Aron Davis <arontd@gmail.com>
2017-08-22 19:25:01 -0400

    6.2.1 ...even though it's kinda dumb.

---
+++ Solutions/_6/_6_2_1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solutions._6
+{
+    /// <summary>
+    /// Given two strings of bits, add them together and return the resulting string of bits.
+    /// </summary>
+    public class _6_2_1
+    {
+        public static string Run(string num1, string num2)
+        {
+            string min;
+            string max;
+
+            if (num1.Length < num2.Length)
+            {
+                min = num1;
+                max = num2;
+            }
+            else
+            {
+                min = num2;
+                max = num1;
+            }
+
+            StringBuilder result = new StringBuilder();
+            int carry = 0;
+            for(int i = 0; i < max.Length; i++)
+            {
+                int bit1 = 0;
+
+                if (i < min.Length)
+                    bit1 = parse(min[min.Length - 1 - i]);
+
+                int bit2 = parse(max[max.Length - 1 - i]);
+
+                int sum = bit1 + bit2 + carry;
+
+                //if our sum will overflow
+                if (sum > 1)
+                {
+                    int num = sum % 2;
+                    carry = sum-1;
+
+                    result.Insert(0, num);
+                }
+                else //no overflow
+                {
+                    int num = sum;
+                    
+                    //reset carry
+                    carry = 0;
+
+                    result.Insert(0, num);
+                }
+            }
+
+            //if we had any carry left at the end
+            while (carry > 0)
+            {
+                result.Insert(0, carry % 2);
+                carry--;
+            }
+
+            return result.ToString();
+        }
+
+        private static int parse(char bit)
+        {
+            if (bit == '0')
+                return 0;
+            else
+                return 1;
+        }
+    }
+}
---
+++ Tests/_6/_6_2_1_Tests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solutions._6;
+using System.Collections.Generic;
+
+namespace Tests
+{
+    [TestClass]
+    public class _6_2_1_Tests
+    {
+        [TestMethod]
+        public void Test()
+        {
+            Assert.IsTrue(_6_2_1.Run("0", "0") == "0");
+            Assert.IsTrue(_6_2_1.Run("1", "0") == "1");
+            Assert.IsTrue(_6_2_1.Run("1", "1") == "10");
+            Assert.IsTrue(_6_2_1.Run("11", "1") == "100");
+            Assert.IsTrue(_6_2_1.Run("1", "11") == "100");
+            Assert.IsTrue(_6_2_1.Run("100", "1") == "101");
+            Assert.IsTrue(_6_2_1.Run("001", "1") == "010");
+        }
+    }
+}
