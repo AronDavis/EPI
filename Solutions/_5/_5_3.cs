5cf5da9129b6837559f77aa128958f6ef8eeb99d
Aron Davis <arontd@gmail.com>
2017-08-22 16:39:22 -0400

    5.3

---
+++ Solutions/_5/_5_3.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Solutions._5
+{
+    /// <summary>
+    /// Write a function that takes a 64-bit word and returns that 64-bit word but with bits reversed.
+    /// </summary>
+    public class _5_3
+    {
+        public static long Run(long data)
+        {
+            //NOTE: book says to precompute and use a lookup table
+            long reversed = 0;
+            for(int i = 0; i < 64; i++)
+            {
+                //push bit of concern to front and isolate
+                long bit = (data >> i) & 1;
+
+                //set it at the appropriate reverse position
+                reversed ^= (bit << (63-i));
+            }
+            return reversed;
+        }
+    }
+}
---
+++ Tests/_5/_5_3_Tests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solutions._5;
+
+namespace Tests
+{
+    [TestClass]
+    public class _5_3_Tests
+    {
+        [TestMethod]
+        public void Test()
+        {
+            Assert.IsTrue(_5_3.Run(0) == 0);
+            Assert.IsTrue(_5_3.Run(1) == ((long)1 << 63));
+            Assert.IsTrue(_5_3.Run(2) == ((long)1 << 62));
+        }
+    }
+}
