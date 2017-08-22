5b04204a558639adf3d62d1ba3fae7e09db3eb59
Aron Davis <arontd@gmail.com>
2017-08-22 17:32:09 -0400

    5.6

---
+++ Solutions/_5/_5_6.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Solutions._5
+{
+    /// <summary>
+    /// Given two positive integers, compute their quotient (aka divide), using only the addition, subtraction, and shift operators.
+    /// </summary>
+    public class _5_6
+    {
+        public static uint Run(uint num1, uint num2)
+        {
+            uint result = 0;
+            while(num1 >= num2)
+            {
+                int power = 1;
+                //prevent overflow
+                while(
+                    (num2 << power) >= (num2 << (power-1)) 
+                    && (num2 << power) <= num1
+                    )
+                {
+                    ++power;
+                }
+
+                result = (uint)1 << (power - 1);
+                num1 -= num2 << (power - 1);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Tests/_5/_5_6_Tests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solutions._5;
+
+namespace Tests
+{
+    [TestClass]
+    public class _5_6_Tests
+    {
+        [TestMethod]
+        public void Test()
+        {
+            Assert.IsTrue(_5_6.Run(0, 1) == 0);
+
+            Assert.IsTrue(_5_6.Run(0, 7) == 0);
+
+            Assert.IsTrue(_5_6.Run(1, 1) == 1);
+
+            Assert.IsTrue(_5_6.Run(3, 3) == 1);
+
+            Assert.IsTrue(_5_6.Run(7, 3) == 2);
+        }
+    }
+}
