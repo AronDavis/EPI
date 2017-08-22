bcbf2422b0013b381bf28de13e2eb7da7a7460bc
Aron Davis <arontd@gmail.com>
2017-08-22 16:52:25 -0400

    5.4

---
+++ Solutions/_5/_5_4.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Solutions._5
+{
+    /// <summary>
+    /// Write a function that takes an unsigned 64-bit integer and a number that
+    ///     a) has the same number of bits flagged
+    ///     b) is a different number
+    ///     c) is as close to the original number as possible while still abiding by A and B
+    /// You can assuming the input will not be 0 or all 1s.
+    /// </summary>
+    public class _5_4
+    {
+        public static long Run(long data)
+        {
+            for(int i = 0; i < 63; i++)
+            {
+                int j = i + 1;
+                //push bit of concern to front and isolate
+                long bit = (data >> i) & 1;
+                long nextBit = (data >> j) & 1;
+
+                //if they differ, swap them
+                if(bit != nextBit)
+                {
+                    //flip the two bits (which is swapping them because they are different)
+                    data ^= (1 << i) | (1 << j);
+                    return data;
+                }
+            }
+
+            return data;
+        }
+    }
+}
---
+++ Tests/_5/_5_4_Tests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solutions._5;
+
+namespace Tests
+{
+    [TestClass]
+    public class _5_4_Tests
+    {
+        [TestMethod]
+        public void Test()
+        {
+            //01 -> 10
+            Assert.IsTrue(_5_4.Run(1) == 2);
+
+            //10 -> 01
+            Assert.IsTrue(_5_4.Run(2) == 1);
+
+            //011 -> 101
+            Assert.IsTrue(_5_4.Run(3) == 5);
+
+            //101 -> 110
+            Assert.IsTrue(_5_4.Run(5) == 6);
+        }
+    }
+}
