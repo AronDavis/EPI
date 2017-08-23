5b519ffea6c5f9d77a3bc50e12c7f9c008a68b5e
Aron Davis <arontd@gmail.com>
2017-08-23 16:02:47 -0400

    6.4

---
+++ Solutions/_6/_6_4.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solutions._6
+{
+    /// <summary>
+    /// Given an array of integers, return whether or not it is possible to advance to the last index in the array.
+    /// You must start from the beginning of the array.
+    /// Advancement is limited to the value at the index you are currently on. 
+    ///     Ex: A[0] = 2, from there you can advance to A[1] or A[2]
+    ///     Ex: A[1] = 3, from there you can advance to A[2], A[3], or A[4]
+    /// </summary>
+    public class _6_4
+    {
+        public static bool Run(int[] data)
+        {
+            int reach = 0;
+
+            //loop through our data until we can go no further or we reach the end of the array
+            for (int i = 0; i <= reach && reach < data.Length; i++)
+                reach = Math.Max(reach, data[i] + i);
+
+            return reach >= data.Length - 1;
+        }
+    }
+}
---
+++ Tests/_6/_6_4_Tests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solutions._6;
+using System.Collections.Generic;
+
+namespace Tests
+{
+    [TestClass]
+    public class _6_4_Tests
+    {
+        [TestMethod]
+        public void Test()
+        {
+            int[] data = new int[]
+            {
+                0
+            };
+            Assert.IsTrue(_6_4.Run(data));
+
+            data = new int[]
+            {
+                0, 0
+            };
+            Assert.IsFalse(_6_4.Run(data));
+
+            data = new int[]
+            {
+                0, 1000
+            };
+            Assert.IsFalse(_6_4.Run(data));
+
+            data = new int[]
+            {
+                2, 0, 0
+            };
+            Assert.IsTrue(_6_4.Run(data));
+
+            data = new int[]
+            {
+                1, 1, 1, 1, 1, 1
+            };
+            Assert.IsTrue(_6_4.Run(data));
+
+            data = new int[]
+            {
+                2, 4, 1, 1, 0, 2, 3
+            };
+            Assert.IsTrue(_6_4.Run(data));
+
+            data = new int[]
+            {
+                3, 3, 1, 0, 2, 0, 1
+            };
+            Assert.IsTrue(_6_4.Run(data));
+
+            data = new int[]
+            {
+                3, 2, 0, 0, 2, 0, 1
+            };
+            Assert.IsFalse(_6_4.Run(data));
+        }
+    }
+}
