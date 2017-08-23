8a279cbc6fa72a9ca63d30ea69b8b3386ef7c131
Aron Davis <arontd@gmail.com>
2017-08-23 15:31:26 -0400

    6.3

---
+++ Solutions/_6/_6_3.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solutions._6
+{
+    /// <summary>
+    /// Given two strings that represent integers, return the integer representing their product (as a string)
+    /// </summary>
+    public class _6_3
+    {
+        public static string Run(string num1, string num2)
+        {
+            bool isPositive = true;
+
+            if (num1[0] == '-')
+            {
+                isPositive = false;
+                num1 = num1.Substring(1);
+            }
+
+            if (num2[0] == '-')
+            {
+                isPositive = !isPositive;
+                num2 = num2.Substring(1);
+            }
+
+            //reverse to make multiplication easier
+            num1 = new string(num1.Reverse().ToArray());
+            num2 = new string(num2.Reverse().ToArray());
+
+            int[] result = new int[num1.Length + num2.Length + 1];
+
+            for (int i = 0; i < num1.Length; i++)
+            {
+                for(int j = 0; j < num2.Length; j++)
+                {
+                    // - '0' to convert to int
+                    result[i + j] += (num1[i] - '0') * (num2[j] - '0');
+                    result[i + j + 1] += result[i + j] / 10;
+                    result[i + j] %= 10;
+                }
+            }
+
+            //remove excess leading 0s
+            int index = num1.Length + num2.Length;
+            while (result[index] == 0 && index != 0)
+                index--;
+
+            StringBuilder sb = new StringBuilder();
+            if (!isPositive && result[index] != 0)
+                sb.Append('-');
+
+            while (index >= 0)
+            {
+                sb.Append(result[index]);
+                index--;
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ Tests/_6/_6_3_Tests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solutions._6;
+using System.Collections.Generic;
+
+namespace Tests
+{
+    [TestClass]
+    public class _6_3_Tests
+    {
+        [TestMethod]
+        public void Test()
+        {
+            Assert.IsTrue(_6_3.Run("0", "0") == "0");
+            Assert.IsTrue(_6_3.Run("0", "-0") == "0");
+            Assert.IsTrue(_6_3.Run("1", "-0") == "0");
+            Assert.IsTrue(_6_3.Run("1", "1") == "1");
+            Assert.IsTrue(_6_3.Run("-1", "1") == "-1");
+            Assert.IsTrue(_6_3.Run("-1", "-1") == "1");
+            Assert.IsTrue(_6_3.Run("3", "7") == "21");
+            Assert.IsTrue(_6_3.Run("3", "-7") == "-21");
+            Assert.IsTrue(_6_3.Run("-7", "-3") == "21");
+        }
+    }
+}
