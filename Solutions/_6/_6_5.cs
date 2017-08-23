e3774ae8ac09d1565c02e1c5b095ffc08a081022
Aron Davis <arontd@gmail.com>
2017-08-23 16:29:08 -0400

    6.5

---
+++ Solutions/_6/_6_5.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solutions._6
+{
+    /// <summary>
+    /// Given an array of integers and a value 'k', update the array so that all occurrences of k are removed and the remaining elements are shifted to the left.
+    /// Return the number of remaining elements.
+    /// There are no requirements as to the values stored beyond the last valid element.
+    /// </summary>
+    public class _6_5
+    {
+        public static int Run(int[] data, int k)
+        {
+            int writeIndex = 0;
+            
+            for (int i = 0; i < data.Length; i++)
+            {
+                int current = data[i];
+                if(current != k)
+                {
+                    data[writeIndex] = current;
+                    writeIndex++;
+                }
+            }
+
+            return writeIndex;
+        }
+    }
+}
---
+++ Tests/_6/_6_5_Tests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solutions._6;
+using System.Collections.Generic;
+
+namespace Tests
+{
+    [TestClass]
+    public class _6_5_Tests
+    {
+        [TestMethod]
+        public void Test()
+        {
+            int[] data = new int[]
+            {
+                5, 3, 7, 11, 2, 3, 13, 5, 7
+            };
+            Assert.IsTrue(_6_5.Run(data, 3) == 7);
+            Assert.IsTrue(data[0] == 5);
+            Assert.IsTrue(data[1] == 7);
+            Assert.IsTrue(data[2] == 11);
+            Assert.IsTrue(data[3] == 2);
+            Assert.IsTrue(data[4] == 13);
+            Assert.IsTrue(data[5] == 5);
+            Assert.IsTrue(data[6] == 7);
+        }
+    }
+}
