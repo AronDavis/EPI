a616ba456f5e97b1bc5316c9e4d25b2a65722bca
Aron Davis <arontd@gmail.com>
2017-08-23 17:24:30 -0400

    6.7

---
+++ Solutions/_6/_6_7.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solutions._6
+{
+    /// <summary>
+    /// Given an array of integers, find the smallest positive number which is not present in the array.
+    /// You do not need to preserve the contents of the array.
+    /// </summary>
+    public class _6_7
+    {
+        public static int Run(int[] data)
+        {
+            int i = 0;
+            while(i < data.Length)
+            {
+                if (data[i] > 0
+                    && data[i] <= data.Length
+                    && data[data[i] - 1] != data[i]
+                    )
+                {
+                    swap(i, data[i] - 1, data);
+                }
+                else
+                    i++;
+            }
+
+            for(i = 0; i < data.Length; i++)
+            {
+                if (data[i] != i + 1)
+                    return i + 1;
+            }
+
+            return data.Length + 1;
+        }
+
+        private static void swap(int i, int j, int[] data)
+        {
+            int temp = data[i];
+            data[i] = data[j];
+            data[j] = temp;
+        }
+    }
+}
---
+++ Tests/_6/_6_7_Tests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solutions._6;
+using System.Collections.Generic;
+
+namespace Tests
+{
+    [TestClass]
+    public class _6_7_Tests
+    {
+        [TestMethod]
+        public void Test()
+        {
+            int[] data = new int[]
+            {
+                3, 4, 0, 2
+            };
+            Assert.IsTrue(_6_7.Run(data) == 1);
+
+            data = new int[]
+            {
+                0, 2, 3, 4,
+            };
+            Assert.IsTrue(_6_7.Run(data) == 1);
+
+            data = new int[]
+            {
+                1, 2, 3, 4, 5, 6, 7
+            };
+            Assert.IsTrue(_6_7.Run(data) == 8);
+
+            data = new int[]
+            {
+                -1
+            };
+            Assert.IsTrue(_6_7.Run(data) == 1);
+
+            data = new int[]
+            {
+                0
+            };
+            Assert.IsTrue(_6_7.Run(data) == 1);
+
+            data = new int[0];
+            Assert.IsTrue(_6_7.Run(data) == 1);
+        }
+    }
+}
