b7330a1be9d66a4374ca3259f06c766375c81282
Aron Davis <arontd@gmail.com>
2017-08-23 16:43:21 -0400

    6.6

---
+++ Solutions/_6/_6_6.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solutions._6
+{
+    /// <summary>
+    /// Given an array of sorted integers, update the array so that all duplicates are removed and the remaining elements are shifted to the left.
+    /// Return the number of remaining elements.
+    /// There are no requirements as to the values stored beyond the last valid element.
+    /// </summary>
+    public class _6_6
+    {
+        public static int Run(int[] data)
+        {
+            int writeIndex = 0;
+            
+            for (int i = 1; i < data.Length; i++)
+            {
+                if(data[writeIndex] != data[i])
+                {
+                    writeIndex++;
+                    data[writeIndex] = data[i];
+                }
+            }
+
+            return writeIndex + 1;
+        }
+    }
+}
---
+++ Tests/_6/_6_6_Tests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solutions._6;
+using System.Collections.Generic;
+
+namespace Tests
+{
+    [TestClass]
+    public class _6_6_Tests
+    {
+        [TestMethod]
+        public void Test()
+        {
+            int[] data = new int[]
+            {
+                1, 2, 2, 3, 3, 3, 4, 4, 4, 4
+            };
+            Assert.IsTrue(_6_6.Run(data) == 4);
+            Assert.IsTrue(data[0] == 1);
+            Assert.IsTrue(data[1] == 2);
+            Assert.IsTrue(data[2] == 3);
+            Assert.IsTrue(data[3] == 4);
+        }
+    }
+}
